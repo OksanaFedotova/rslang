af32d4196b7728c9964ca77ac32262522a26e969
Oksana Fedotova <oxana-fedotova@yandex.ru>
2022-08-21 14:44:04 +0300

    fix: fix  className, remove excess files

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Menu from '../Menu/Menu';
+import AuthForm from "../../components/Auth-form/Authform";
+import Validation from "../../components/Auth-form/Validation";
 import logo  from '../../assets/logo.png'
-import Button from '../Button/Button';
 import './Header.css';
 
 const Header = () => {
-    const handleClickButton = () => {
-      alert("показывать форму регистрации")
-    };
+
+const [modalActive, setModalActive] = React.useState<boolean | undefined>(false);
 
   return (
     <div>
@@ -17,7 +17,12 @@ const Header = () => {
            <p className='logo-text'>RSLang</p>
         </div>
         <Menu></Menu>
-        <Button title='Войти' handleClick={handleClickButton} className="header-button"></Button>
+        <button className="header-button" onClick={() => setModalActive(true) }>Войти</button>
+        <AuthForm active={modalActive} setActive={setModalActive}>
+            <form className="input-form" action="">
+                <Validation/>
+            </form>
+          </AuthForm> 
       </header>
     </div>
   )
