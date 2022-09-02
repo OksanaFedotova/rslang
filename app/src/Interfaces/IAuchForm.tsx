9194da8ecd58c44756a60a46c782b679c2a15567
SashaJozwiak <wmdz88@gmail.com>
2022-09-02 04:55:45 +0300

    feat: stats and keyboard controls

--- app/src/Interfaces/IAuchForm.tsx
+++ app/src/Interfaces/IAuchForm.tsx
@@ -1,17 +1,6 @@
 interface IAuthForm {
-    key: string
-    image: string;
-    textExample: string;
-    textExampleTranslate: string;
-    textMeaningTranslate: string;
-    textMeaning: string;
-    transcription: string;
-    word: string;
-    word2: string;
-    word3: string;
-    word4: string;
-    wordTranslate: string;
-    audio: string;
-    audioWord: string;
-  }
-  export default IAuthForm;
+  wordsCard: string[];
+  KeyCode: number;
+
+}
+export default IAuthForm;
