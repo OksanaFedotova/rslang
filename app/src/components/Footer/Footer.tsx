b46d8a4aa2cd3a3eb348d05556d0421d9cd53c44
Oksana Fedotova <oxana-fedotova@yandex.ru>
2022-09-23 20:06:01 +0300

    fix: fix styles

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -3,22 +3,27 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-      <footer className="footer">
-             <div>
-              <a className='rs-link' href='https://rs.school/js/'>
-                <img src={'https://rs.school/images/rs_school_js.svg'} width='70px'></img>
-              </a>
-             </div>
-             <div className="center-block">
-              <a className="footer-link" href='https://github.com/OksanaFedotova'>Оксана</a>
-              <a className="footer-link" href='https://github.com/sashajozwiak'>Александр</a>
-              <a className="footer-link" href='https://github.com/daryadak'>Дарья</a>
-             </div>
-             <div>
-              <p>2022</p>
-             </div>
-        </footer>
-  )
-}
-
+    <footer className="footer">
+      <div>
+        <a className="rs-link" href="https://rs.school/js/">
+          <img src={"https://rs.school/images/rs_school_js.svg"} />
+        </a>
+      </div>
+      <div className="center-block">
+        <a className="footer-link" href="https://github.com/OksanaFedotova">
+          Оксана
+        </a>
+        <a className="footer-link" href="https://github.com/sashajozwiak">
+          Александр
+        </a>
+        <a className="footer-link" href="https://github.com/daryadak">
+          Дарья
+        </a>
+      </div>
+      <div>
+        <p>2022</p>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
